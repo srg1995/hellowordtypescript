d6ebf81fb38e4f511156e941ebb78c324a22da4a
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-19 10:20:59 +0100

    computed data

---
+++ src/components/ComputerExample.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input type="text" v-model="message" />
+    <p>PRECIO: {{ message }}</p>
+    <p>IVA :{{ reverse }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref, computed, ComputedRef } from "vue";
+const IVA = 0.21;
+let message: Ref<number> = ref(0);
+let reverse: ComputedRef<number> = computed(() => {
+  const iva = message.value * IVA;
+  return iva;
+});
+</script>
+
+<style scoped></style>
