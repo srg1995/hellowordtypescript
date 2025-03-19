f08bc71a3213c95f7edbef88f80775656081ecaf
Sergio Sacristan Santero <srg_ek@hotmail.com>
2025-03-19 09:18:19 +0100

    life cycle

---
+++ src/components/CycleOption.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Hola Mundo</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CycleOption",
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/CycleOptionComposition.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, onUpdated } from "vue";
+onMounted(() => {
+  console.log("mounted");
+});
+onUpdated(() => {
+  console.log("updated");
+});
+</script>
+
+<style scoped></style>
